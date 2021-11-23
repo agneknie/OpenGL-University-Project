7e479150d7cf591ef2360bb5e7ff7b56129a9e92
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2021-11-23 12:56:19 +0000

    Figure classes from tutorial

---
+++ src/figures/Sphere.java
@@ -0,0 +1,63 @@
+package figures;
+
+/**
+ * Class taken from COM3503 Online Tutorial Materials
+ * by Dr Steve Maddock at The University of Sheffield, 2021.
+ *
+ *  Modified to better suit the assignment for COM3503
+ *  by Agne Knietaite, 2021.
+ */
+public final class Sphere {
+    private static final int XLONG = 30;
+    private static final int YLAT = 30;
+
+    public static final float[] vertices = createVertices();
+    public static final int[] indices = createIndices();
+
+    private static float[] createVertices() {
+        double r = 0.5;
+        int step = 8;
+        float[] vertices = new float[XLONG*YLAT*step];
+
+        for (int j = 0; j<YLAT; ++j) {
+            double b = Math.toRadians(-90+180*(double)(j)/(YLAT-1));
+
+            for (int i = 0; i<XLONG; ++i) {
+                double a = Math.toRadians(360*(double)(i)/(XLONG-1));
+                double z = Math.cos(b) * Math.cos(a);
+                double x = Math.cos(b) * Math.sin(a);
+                double y = Math.sin(b);
+
+                int base = j*XLONG*step;
+
+                vertices[base + i*step+0] = (float)(r*x);
+                vertices[base + i*step+1] = (float)(r*y);
+                vertices[base + i*step+2] = (float)(r*z);
+                vertices[base + i*step+3] = (float)x;
+                vertices[base + i*step+4] = (float)y;
+                vertices[base + i*step+5] = (float)z;
+                vertices[base + i*step+6] = (float)(i)/(float)(XLONG-1);
+                vertices[base + i*step+7] = (float)(j)/(float)(YLAT-1);
+            }
+        }
+        return vertices;
+    }
+
+    private static int[] createIndices() {
+        int[] indices = new int[(XLONG-1)*(YLAT-1)*6];
+
+        for (int j = 0; j<YLAT-1; ++j) {
+            for (int i = 0; i<XLONG-1; ++i) {
+                int base = j*(XLONG-1)*6;
+
+                indices[base + i*6+0] = j*XLONG+i;
+                indices[base + i*6+1] = j*XLONG+i+1;
+                indices[base + i*6+2] = (j+1)*XLONG+i+1;
+                indices[base + i*6+3] = j*XLONG+i;
+                indices[base + i*6+4] = (j+1)*XLONG+i+1;
+                indices[base + i*6+5] = (j+1)*XLONG+i;
+            }
+        }
+        return indices;
+    }
+}
---
+++ src/figures/TwoTriangles.java
@@ -0,0 +1,23 @@
+package figures;
+
+/**
+ * Class taken from COM3503 Online Tutorial Materials
+ * by Dr Steve Maddock at The University of Sheffield, 2021.
+ *
+ * Modified to better suit the assignment for COM3503
+ * by Agne Knietaite, 2021.
+ */
+public final class TwoTriangles {
+    public static final float[] vertices = {
+            // position, colour, texture coordinates
+            -0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,  // top left
+            -0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // bottom left
+            0.5f, 0.0f,  0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // bottom right
+            0.5f, 0.0f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f   // top right
+    };
+
+    public static final int[] indices = {
+            0, 1, 2,
+            0, 2, 3
+    };
+}
